c01e63749f76ecb2f7ace67649a8ed018370790a
장보경 <bonnie8623@gmail.com>
2026-02-13 12:08:16 +0900

    Feat: prompt 생성 useCase 구현

---
+++ src/main/java/rag/markdown_creator/application/service/GeneratePromptService.java
@@ -0,0 +1,42 @@
+package rag.markdown_creator.application.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.azure.openai.AzureOpenAiChatOptions;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import rag.markdown_creator.application.port.in.GeneratePromptUseCase;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GeneratePromptService implements GeneratePromptUseCase {
+
+    @Value("prompt.system-message")
+    private final String systemMessage;
+
+    @Override
+    public Prompt execute(String userMessage) {
+        List<Message> messages = Arrays.asList(
+                new SystemMessage(systemMessage),
+                new UserMessage(userMessage)
+        );
+        return Prompt.builder()
+                .chatOptions(getOptions())
+                .messages(messages)
+                .build();
+    }
+
+    private AzureOpenAiChatOptions getOptions() {
+        return AzureOpenAiChatOptions.builder()
+                .deploymentName("gpt-4.0")
+                .temperature(0.5)
+                .build();
+    }
+}
