3938cdcb3ea3f50075bf2d0d1151bbfb27a6bac3
장보경 <bonnie8623@gmail.com>
2026-02-13 18:25:40 +0900

    Feat: 문서 다운로드 api 구현 완료

--- src/main/java/rag/markdown_creator/adapter/in/controller/DownloadController.java
+++ src/main/java/rag/markdown_creator/adapter/in/controller/DownloadController.java
@@ -2,13 +2,42 @@ package rag.markdown_creator.adapter.in.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rag.markdown_creator.adapter.in.dto.DownloadDocumentRequestDto;
+import rag.markdown_creator.application.port.in.DownloadDocumentUseCase;
+import rag.markdown_creator.application.vo.DownloadFile;
+import rag.markdown_creator.application.vo.MarkdownDocument;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class DownloadController {
 
-    // 문서 다운로드
+    private final DownloadDocumentUseCase downloadDocumentUseCase;
 
+    // 문서 다운로드
+    @PostMapping("/api/v1/download")
+    public ResponseEntity<Resource> download(List<DownloadDocumentRequestDto> requestDtos) {
+        List<MarkdownDocument> documents = requestDtos.stream()
+                .map(DownloadDocumentRequestDto::toVo).toList();
+        DownloadFile result = downloadDocumentUseCase.execute(documents);
+
+        if(result == null) {
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.ok()
+                .contentType(result.getContentType())
+                .contentLength(result.getContent().length)
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + result.getFileName() + "\"")
+                .body(new ByteArrayResource(result.getContent()));
+    }
 }
